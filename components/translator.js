4823876ad0b7d0f58e844d2bee64d880bcdb778f
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-20 14:38:37 +0000

    Started Logic Changes

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,7 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
-
+  
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,20 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
+      let text = req.body.text;
+      let locale = req.body.locale;
+
+      if(text == undefined || locale == undefined){
+        return res.json({ error: 'Required field(s) missing' });
+      }
       
+      if(text == ""){
+        return res.json({ error: 'No text to translate' });
+      }
+      
+      if(locale != "american-to-british" && locale != "british-to-american")
+      {
+        return res.json({ error: 'Invalid value for locale field' });
+      }
     });
 };
